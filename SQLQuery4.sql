d40b97cc44545e2d86ca159fba73d3c4f94e5ccf
ishansi-abjima <119054363+ishansi-abjima@users.noreply.github.com>
2023-01-18 17:38:47 +0530

    Add files via upload

---
+++ SQLQuery4.sql
@@ -0,0 +1,57 @@
+SELECT 30+20;
+
+SELECT 30-20;
+
+SELECT 17%5;
+
+SELECT * FROM Customers
+
+WHERE CustomerID = 1;
+
+
+SELECT * FROM Customers
+
+WHERE CustomerID <> 1;
+
+
+SELECT * FROM Customers
+
+WHERE CustomerName LIKE 'b_a_c';
+
+
+SELECT * FROM Customers
+
+WHERE CustomerName LIKE '[bsp%]';
+
+
+SELECT * FROM Customers
+
+WHERE CustomerID LIKE '[a-c]%';
+
+
+SQL HOSTING
+
+--if you want your sql server to store and retrieve the data from the database then your web server must have access to the the database this is called hosting 
+
+--sql injection is the placement of malicious code into sql database via web page input it places the malicious code into your sql statements which might destroy your databse 
+
+--hyperlink contains the link to all audio and video files including the web pages 
+
+--ole object stores all audio and video files 
+
+--lookup wizard contains the dropdown menu 
+
+--text contains the mixtue of text and numbers maximum 255 characters
+
+--memo stores the large amount of data 
+
+--you cannot sort a memo field but can be searchable
+
+--byte datatype stores whole numbers between 0 and 255
+
+--single datatype stores the single precision floating point 
+
+--double datatype stores the floating point number with double precision 
+
+--timestamp stores the unique value of a number that gets updated every time a row is created or modified .. timestamp value is based on the internal clock and does not corresponds to the real time 
+
